8e818c29c1f377c969a21dee78e302d42c9e9789
Ayun14 <127943296+Ayun14@users.noreply.github.com>
2024-12-17 23:26:57 +0900

    PoolManager 제거

--- GameJam_2024/Assets/99.UAPT/02.Pool/Editor/GondrSplitView.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine.UIElements;
-
-public class GondrSplitView : TwoPaneSplitView
-{
-    public new class UxmlFactory : UxmlFactory<GondrSplitView, UxmlTraits>{}
-    public new class UxmlTraits : TwoPaneSplitView.UxmlTraits {}
-}
