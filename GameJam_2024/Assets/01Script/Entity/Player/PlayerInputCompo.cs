24604df8f73c90d77a48ddf2a7d0d6bc8f07e4a9
Ayun14 <127943296+Ayun14@users.noreply.github.com>
2024-12-18 10:11:44 +0900

    Add | Player FSM base

---
+++ GameJam_2024/Assets/01Script/Entity/Player/PlayerInputCompo.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputCompo : MonoBehaviour, IEntityComponent
+{
+    public event Action<Vector2> OnMovementKeyEvent;
+    public event Action OnJumpKeyEvent;
+
+    public event Action OnMouseDownEvent;
+    public event Action<Vector2> OnMouseStayEvent;
+    public event Action OnMouseUpEvent;
+
+    private bool _isMouseDown = false;
+
+    private Player _player;
+
+    public void Initialize(Entity entity)
+    {
+        _player = entity as Player;
+    }
+
+    private void Update()
+    {
+        MovementInput();
+        JumpInput();
+        MouseInput();
+    }
+
+    private void JumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnJumpKeyEvent?.Invoke();
+        }
+    }
+
+    private void MouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnMouseDownEvent?.Invoke();
+            _isMouseDown = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            OnMouseUpEvent?.Invoke();
+            _isMouseDown = false;
+        }
+
+        if (true == _isMouseDown)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            OnMouseStayEvent?.Invoke(mousePos);
+        }
+    }
+
+    private void MovementInput()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(x, 0);
+        OnMovementKeyEvent?.Invoke(movement);
+    }
+}
