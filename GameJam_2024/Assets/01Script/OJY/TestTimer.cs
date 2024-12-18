44369fa4c9d377f1970e842274a4bd512928b139
zhdltl601 <zhdltl601@gmail.com>
2024-12-19 01:29:48 +0900

    TestTimer

---
+++ GameJam_2024/Assets/01Script/OJY/TestTimer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTimer : MonoBehaviour
+{
+    private float timer = 0;
+    //private void OnTrestFunc()
+    //{
+    //    print(Time.time - timer);
+    //    timer = Time.time;
+    //}
+}
