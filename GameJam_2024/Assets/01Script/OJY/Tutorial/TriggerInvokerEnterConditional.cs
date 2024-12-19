0d476c04113999b093cd46076f86466d086c4ab9
zhdltl601 <zhdltl601@gmail.com>
2024-12-19 17:37:55 +0900

    Added Tutorial

---
+++ GameJam_2024/Assets/01Script/OJY/Tutorial/TriggerInvokerEnterConditional.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerInvokerEnterConditional : InvokerBase
+{
+    private int cnt = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (cnt > 1) InvokeTrigger();
+        cnt++;
+    }
+}
---
+++ GameJam_2024/Assets/01Script/OJY/Tutorial/Triggerable/Tutorial_RespawnBullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_RespawnBullet : MonoBehaviour
+{
+    [SerializeField] private List<Transform> bulletSpawnTransform;
+    private List<BaseBullet> spawnedBullets = new();
+    [SerializeField] private BaseBullet bulletToSpawn;
+    [SerializeField] private GameObject spawnParticle;
+    public void OnEnter()
+    {
+        for (int i = spawnedBullets.Count - 1; i >= 0; i--)
+        {
+            var result = spawnedBullets[i];
+            if (result == null) continue;
+            result.Kill();
+        }
+        spawnedBullets.Clear();
+        foreach (var item in bulletSpawnTransform)
+        {
+            BaseBullet bullet = Instantiate(bulletToSpawn, item.position, Quaternion.identity, item);
+            spawnedBullets.Add(bullet);
+        }
+    }
+}
