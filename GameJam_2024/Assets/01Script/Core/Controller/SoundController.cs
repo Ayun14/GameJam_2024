12c84ef25f79b643fe80c0dff5ac894bf0fb5d1c
gongsy1221 <gongsy0724@naver.com>
2024-12-20 08:40:54 +0900

    Add BGM

--- GameJam_2024/Assets/01Script/Core/Controller/SoundController.cs
+++ GameJam_2024/Assets/01Script/Core/Controller/SoundController.cs
@@ -8,12 +8,6 @@ public class SoundController : MonoSingleton<SoundController>
     [SerializeField] private List<AudioClip> _bgmClips;
     [SerializeField] private List<AudioClip> _sfxClips;
 
-    private void Start()
-    {
-        if (SceneManager.GetSceneByName("MainScene") == SceneManager.GetActiveScene())
-            PlayBGM(1);
-    }
-
     public void PlayBGM(int bgmNum)
     {
         EazySoundManager.PlayMusic(_bgmClips[bgmNum]);
