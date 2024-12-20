20ae220fb38b65597e87f8fb8530e98bd03805d3
Ayun14 <127943296+Ayun14@users.noreply.github.com>
2024-12-20 12:52:33 +0900

    Build | 최종본

--- GameJam_2024/Assets/01Script/Object/Portal.cs
+++ GameJam_2024/Assets/01Script/Object/Portal.cs
@@ -6,7 +6,10 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour
 {
     [SerializeField] private CanvasGroup _fadeCanvas;
-
+    private void Start()
+    {
+        SoundController.Instance.PlayBGM(1);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out Player player))
