868ade9878ad6070731f187868b11b940a6d87d9
Ayun14 <127943296+Ayun14@users.noreply.github.com>
2025-01-05 23:37:08 +0900

    Test | Login And Register Test

--- GameJam_2024/Assets/01Script/RankingSystem/UserManager.cs
+++ GameJam_2024/Assets/01Script/RankingSystem/UserManager.cs
@@ -1,8 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UserManager : MonoBehaviour
 {
+    [Header("Main Pannel")]
+    [SerializeField] private GameObject _loginPannel; 
+    [SerializeField] private GameObject _registerPannel;
 
+    #region Main Buttons
+
+    public void LoginPannelOn()
+    {
+        _loginPannel.SetActive(true);
+    }
+    public void LoginPannelOff()
+    {
+        _loginPannel.SetActive(false);
+    }
+    public void RegisterPannelOn()
+    {
+        _registerPannel.SetActive(true);
+    }
+    public void RegisterPannelOff()
+    {
+        _registerPannel.SetActive(false);
+    }
+
+    #endregion
 }
